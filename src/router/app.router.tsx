519545273abeef41dcd681a719a98fcd57cb9f1e
SmailFahmi10 <smailfahmi984@gmail.com>
2025-12-27 21:12:04 +0100

    Cambios para el despliegue

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -6,12 +6,13 @@ import { HomePage } from "@/heroes/pages/home/HomePage";
 
 //import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { lazy } from "react";
-import { createBrowserRouter, Navigate } from "react-router";
+import { createHashRouter, Navigate } from "react-router";
 
 const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 const AdminPage = lazy(() => import("@/admin/pages/AdminPage"));
 
-export const appRouter = createBrowserRouter([
+// export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
   {
     path: "/",
     element: <HeroesLayout />,
